181cf215d6621c8a8108407636d3166d3550449e
Boy Baukema <bbaukema@veracode.com>
2021-11-18 14:19:37 +0100

    Initial generation

---
+++ src/test/java/io/veracode/asc/bbaukema/springbootcwe117/SpringBootCwe117ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.veracode.asc.bbaukema.springbootcwe117;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCwe117ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
